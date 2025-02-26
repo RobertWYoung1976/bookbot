535b2a6e08c9a45be6cf751bf7f6a637a5a97969
Robert Young <robertwyYoung1976@gmail.com>
2025-02-26 12:19:06 -0600

    Added sysarg

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import sys
+
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
